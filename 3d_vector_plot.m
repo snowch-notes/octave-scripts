05092a8acfd8417d632c27b89c2d45efa19bdb7d
Chris Snow <chsnow123@gmail.com>
2017-02-05 15:32:01 +0000

    Update 3d_vector_plot.m

--- 3d_vector_plot.m
+++ 3d_vector_plot.m
@@ -1,6 +1,23 @@
-beg_pt = [0 0 0];
-fin_pt = [1 2 3];
+% Draw 3d arrow from x0,y0,z0 to x1,y1,z1
+%
+% parameters:
+%    V = [ x0 x1; y0 y1; z0 z1 ]
+%
+% 
+function quiver3_vect( V )
+  quiver3( V(1, 1), V(2, 1), V(3, 1), V(1, 2), V(2, 2), V(3, 2) )
+endfunction
 
-v = horzcat( beg_pt, fin_pt );
+beg_pt = [0 0 0]';
+fin_pt = [1 2 3]';
 
-quiver3( v(1), v(2), v(3), v(4), v(5), v(6) )
+vect = horzcat( beg_pt, fin_pt );
+
+% >> vect
+% vect =
+%
+%   0   1
+%   0   2
+%   0   3
+
+quiver3_vect( vect )
